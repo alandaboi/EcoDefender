727622605376a6ae57d83ad9a4bce4e084cbb325
Alan Xiao <alanxiao2013@gmail.com>
2021-07-05 16:00:29 +0800

    test didn't work

--- assets/tf.js
+++ assets/tf.js
@@ -1,45 +1,16 @@
-$("#image-selector").change(function () {
-	let reader = new FileReader();
-	reader.onload = function () {
-		let dataURL = reader.result;
-		$("#selected-image").attr("src", dataURL);
-		$("#prediction-list").empty();
-	}
+let net;
 
-	let file = $("#image-selector").prop('files')[0];
-	reader.readAsDataURL(file);
-});
+async function app() {
+  console.log('Loading mobilenet..');
 
-let model;
-$(document).ready(async function () {
-	$('.progress-bar').show();
-	model = await tf.loadGraphModel('assets/model.json');
-	$('.progress-bar').hide();
-});
+  // Load the model.
+  net = await mobilenet.load();
+  console.log('Successfully loaded model');
 
+  // Make a prediction through the model on our image.
+  const imgEl = document.getElementById('img');
+  const result = await net.classify(imgEl);
+  console.log(result);
+}
 
-
-$("#predictBtn").click(async function () {
-	let image = $('#selected-image').get(0);
-	
-	let pre_image = tf.browser.fromPixels(image, 3)
-		.resizeNearestNeighbor([224, 224])
-		.expandDims()
-		.toFloat(); 
-	let predict_result = await model.predict(pre_image).data();
-	console.log(predictions);
-	let order = Array.from(predict_result)
-		.map(function (p, i) { 
-			return {
-				probability: p,
-				className: Result[i] 
-			};
-		}).sort(function (a, b) {
-			return b.probability - a.probability;
-		}).slice(0, 2);
-
-	$("#list").empty();
-	order.forEach(function (p) {
-		$("#list").append(`<li>${p.className}: ${parseInt(Math.trunc(p.probability * 100))} %</li>`);
-	});
-});
+app();
